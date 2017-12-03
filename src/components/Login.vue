f5a1df6feb3442b06bc5189aec086917009fbc07
zc <zachcaceres@gmail.com>
2017-12-03 11:40:10 -0500

    add login wireframe and config vuefire

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+   <!-- The code below initializes the sign-in widget from FirebaseUI web. -->
+  <div id="firebaseui-auth-container">
+  </div>
+</template>
+
+
+<script>
+// TODO: AUTH HERE
+export default {
+  name: 'Login'
+}
+</script>
+
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Game from '@/components/Game'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/game',
       name: 'Game',
       component: Game
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
