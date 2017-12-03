c6a62a418fde969e81b058100766633c81ced79e
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2017-12-03 11:32:01 -0500

    feat: Display values

---
+++ src/components/Values.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ getValues() }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Values',
+  data () {
+    return {}
+  },
+  methods: {
+    getValues: function() {
+      let a = [1,2,3,4,5];
+      let round = 1;
+      let str = '';
+      for (let i = 0; i < round; i++) {
+        str += a[i] + ' ';
+      }
+      for (let i = round; i < a.length; i++) {
+        str += 'X ';
+      }
+      return str;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Values from '@/components/Values'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Values',
+      name: 'Values',
+      component: Values
     }
   ]
 })
