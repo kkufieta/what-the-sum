a5ad497a7bd3de65c905fdc2bcde1ceb599325bc
zc <zachcaceres@gmail.com>
2017-12-03 11:10:30 -0500

    add bid offer and main game view

---
+++ src/components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="game">
+    <BidOffer></BidOffer>
+  </div>
+</template>
+
+<script>
+import BidOffer from './BidOffer'
+
+export default {
+  name: 'Game',
+  components: {
+    BidOffer
+  }
+}
+</script>
+
+<style>
+
+</style>
