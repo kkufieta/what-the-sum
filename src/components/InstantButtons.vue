5a3721e3dd703218c1d5bbe3a1371a69f3df27e2
zc <zachcaceres@gmail.com>
2017-12-12 21:58:39 -0500

    feat: instant buy and sell transactions

--- src/components/InstantButtons.vue
+++ src/components/InstantButtons.vue
@@ -17,25 +17,42 @@ const db = firebase.database()
 export default {
   name: 'BidOffer',
   data () {
-    return {}
+    return {
+      userId: '',
+      user: {}
+    }
   },
   firebase: {
     currentBids: db.ref('games/game1/current_bids'),
     currentOffers: db.ref('games/game1/current_offers')
   },
+  created () {
+    this.user = firebase.auth().currentUser
+    if (this.user) {
+      this.userId = this.user.uid
+    }
+  },
   methods: {
     getMin (arr) {
       return Math.min.apply(Math, arr)
     },
+    firstSellerNotMe (ref, val) {
+      const [best] = ref.filter(el => el['.key'] === val.toString())
+      const otherUser = Object.keys(best).filter(key => key !== '.key')
+        .filter(userId => userId !== this.userId)
+      if (!otherUser.length) return// ERROR HANDLE HERE
+      return otherUser[0]
+    },
     sendBuy (e) {
-      const lowestOffer = this.currentOffers.map(offer => offer['.key'])
-      console.log(this.getMin(lowestOffer))
-      // this.$firebaseRefs.currentBids.push({ bidPrice: 'userOne' })
+      const lowestOffer = this.getMin(this.currentOffers.map(offer => offer['.key']))
+      const offerer = this.firstSellerNotMe(this.currentOffers, lowestOffer)
+      this.$firebaseRefs.currentOffers.child(lowestOffer).child(offerer).set(null)
+      // PROCESS TRANSACTIONS HERE
     },
     sendSell (e) {
-      const lowestBid = this.currentBids.map(bid => bid['.key'])
-      console.log(this.getMin(lowestBid))
-      // console.log('i sent', this.newOffer.value)
+      const lowestBid = this.getMin(this.currentBids.map(bid => bid['.key']))
+      const bidder = this.firstSellerNotMe(this.currentBids, lowestBid)
+      this.$firebaseRefs.currentBids.child(lowestBid).child(bidder).set(null)
     }
   },
   computed: {
