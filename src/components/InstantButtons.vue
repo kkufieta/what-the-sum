9b3b0ab2fa1c1682b10e030f633cab91c59291c8
zc <zachcaceres@gmail.com>
2017-12-12 21:35:16 -0500

    feat: databinding on instant sell and buy buttons

--- src/components/InstantButtons.vue
+++ src/components/InstantButtons.vue
@@ -1,29 +1,51 @@
 <template>
   <div id="buy-sell" class="row">
     <div class="col s12 button-div">
-      <button class="waves-effect waves-light btn" @click.stop="sendBuy">Buy</button>
+      <button class="waves-effect waves-light btn" @click.stop="sendBuy">Buy at {{lowestOffer}}</button>
     </div>
     <div class="col s12 button-div">
-      <button class="waves-effect waves-light btn" @click.stop="sendSell">Sell</button>
+      <button class="waves-effect waves-light btn" @click.stop="sendSell">Sell at {{lowestBid}}</button>
     </div>
     <!-- USER's OWN bid and offer in yellow or green -->
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+const db = firebase.database()
+
 export default {
   name: 'BidOffer',
   data () {
     return {}
   },
+  firebase: {
+    currentBids: db.ref('games/game1/current_bids'),
+    currentOffers: db.ref('games/game1/current_offers')
+  },
   methods: {
+    getMin (arr) {
+      return Math.min.apply(Math, arr)
+    },
     sendBuy (e) {
+      const lowestOffer = this.currentOffers.map(offer => offer['.key'])
+      console.log(this.getMin(lowestOffer))
       // this.$firebaseRefs.currentBids.push({ bidPrice: 'userOne' })
-      // TODO: firebase post buy
     },
     sendSell (e) {
+      const lowestBid = this.currentBids.map(bid => bid['.key'])
+      console.log(this.getMin(lowestBid))
       // console.log('i sent', this.newOffer.value)
-      // TODO: firebase post sell
+    }
+  },
+  computed: {
+    lowestOffer: function () {
+      const lowestOffer = this.currentOffers.map(offer => offer['.key'])
+      return this.getMin(lowestOffer)
+    },
+    lowestBid: function () {
+      const lowestBid = this.currentBids.map(bid => bid['.key'])
+      return this.getMin(lowestBid)
     }
   }
 }
